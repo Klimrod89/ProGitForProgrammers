4ad1e43bf225de4adaabc2f75e076158b9171419
Yassir C <koutoukale+VSGit@gmail.com>
2023-12-25 01:12:20 -0500

    New Class Book added to Book Branch

---
+++ ProGitForProgrammers/ConsoleUI/Book.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleUI;
+public class Book
+{
+    public string Title { get; set; }
+    public List<string> Authors { get; set; }
+    public DateTime PublicationDate { get; set; }
+}
